22ef4914e4de611cb3924f62b556eb1c60ae6d8c
SeikaHirori <SeikaHirori@outlook.com>
2023-05-01 13:50:29 -0700

    Created folder for working on Part 1

---
+++ Drawing/part1/impl_Part1.swift
@@ -0,0 +1,20 @@
+//
+//  impl_Part1.swift
+//  Drawing
+//
+//  Created by Seika Hirori on 5/1/23.
+//
+
+import SwiftUI
+
+struct impl_Part1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct impl_Part1_Previews: PreviewProvider {
+    static var previews: some View {
+        impl_Part1()
+    }
+}
