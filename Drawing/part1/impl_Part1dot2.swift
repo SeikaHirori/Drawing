ff3d999efee87999606da5a88372cb6f5b4a6e34
SeikaHirori <SeikaHirori@outlook.com>
2023-05-01 14:24:54 -0700

    created Triangle struct

--- Drawing/part1/impl_Part1dot2.swift
+++ Drawing/part1/impl_Part1dot2.swift
@@ -9,12 +9,28 @@ import SwiftUI
 
 struct impl_Part1dot2: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        return VStack {
+            Text("Hello, World!")
+        }
     }
 }
 
 struct impl_Part1dot2_Previews: PreviewProvider {
     static var previews: some View {
         impl_Part1dot2()
+    }
+}
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        
+        return path
     }
 }
