2891388f9ed65377aa15956f6da0e150408624db
SeikaHirori <SeikaHirori@outlook.com>
2023-05-01 14:11:00 -0700

    Added Path to draw triangle, and filled it

--- Drawing/part1/impl_Part1.swift
+++ Drawing/part1/impl_Part1.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct impl_Part1: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        return VStack {
+            Path { path in
+                path.move(to: CGPoint(x: 200, y: 100))
+                path.addLine(to: CGPoint(x: 100, y: 300))
+                path.addLine(to: CGPoint(x: 300, y: 300))
+                path.addLine(to: CGPoint(x: 200, y: 100))
+            }
+            .fill(Color.blue)
+//            .stroke(.blue, lineWidth: 10)
+        }
     }
 }
 
