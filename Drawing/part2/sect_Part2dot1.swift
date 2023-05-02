3753bce35c4d34b9d474853eb9a3635a5b653a96
SeikaHirori <SeikaHirori@outlook.com>
2023-05-02 14:52:44 -0700

    Added Flower struct

--- Drawing/part2/sect_Part2dot1.swift
+++ Drawing/part2/sect_Part2dot1.swift
@@ -32,6 +32,29 @@ struct Flower: Shape {
     
     
     func path(in rect: CGRect) -> Path {
-        <#code#>
+        
+        // The path that will hold all petals
+        var path = Path()
+        
+        // Count from 0 up to pi * 2, moving up pi / 8 each time
+        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
+            
+            // rotate the petal by the current value of our loop
+            let rotation: CGAffineTransform = CGAffineTransform(rotationAngle: number)
+            
+            // move the petal to be the center of our view
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            
+            // create a path for this petal using our properties plus a fixed Y and height
+            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
+            
+            // apply our rotation/ position transformation to the petal
+            let rotatedPetal = originalPetal.applying(position)
+            
+            path.addPath(rotatedPetal)
+        }
+        
+        // now send the main path back
+        return path
     }
 }
