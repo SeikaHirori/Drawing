2da0a055aede365f653e03cbcdde81586e36c47d
SeikaHirori <SeikaHirori@outlook.com>
2023-05-04 14:03:13 -0700

    Started section 2.2

---
+++ Drawing/part2/sect_Part2dot2.swift
@@ -0,0 +1,20 @@
+//
+//  sect_Part2dot2.swift
+//  Drawing
+//
+//  Created by Seika Hirori on 5/4/23.
+//
+
+import SwiftUI
+
+struct sect_Part2dot2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct sect_Part2dot2_Previews: PreviewProvider {
+    static var previews: some View {
+        sect_Part2dot2()
+    }
+}
