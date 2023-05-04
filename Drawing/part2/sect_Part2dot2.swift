36f66f86ccb39391e0a0379bf5c4a00a2559ec6d
SeikaHirori <SeikaHirori@outlook.com>
2023-05-04 14:31:17 -0700

    Added scroll view to demo all modifiers

--- Drawing/part2/sect_Part2dot2.swift
+++ Drawing/part2/sect_Part2dot2.swift
@@ -9,42 +9,47 @@ import SwiftUI
 
 struct sect_Part2dot2: View {
     var body: some View {
-        
-        return VStack {
+        return GeometryReader { outerGeo in
+            let outerGeoWidth = outerGeo.size.width
+            let outerGeoHeight = outerGeo.size.height
             
-            // Background modifier
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                .frame(width: 300, height: 300)
-                .background(Color.red)
+            ScrollView{
+                VStack {
+                    // Background modifier
+                    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                        .frame(width: 300, height: 300)
+                        .background(Color.red)
+                        .padding()
+                    
+                    // Border modifier
+                    Text("hello world :3")
+                        .frame(width: 300, height: 300)
+                        .border(.red, width: 30)
+                        .padding()
+                    
+                    // Use image for background
+                    Text("hello world :3")
+                        .frame(width: 300, height: 300)
+                        .foregroundColor(.white)
+                        .background(
+                            GeometryReader { geo in
+                                let geoWidth = geo.size.width
+                                let geoHeight = geo.size.height
+                                
+                                Image("pexels-pixabay-68474")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: geoWidth * 0.8, height: geoHeight * 0.8)
+                                    .frame(width: geoWidth, height: geoHeight)
+                            }
+                        )
+                        .background(.blue)
+                        .padding()
+                }
+                .frame(width: outerGeoWidth * 0.90, height: outerGeoHeight * 0.90)
                 .padding()
-            
-            // Border modifier
-            Text("hello world :3")
-                .frame(width: 300, height: 300)
-                .border(.red, width: 30)
-                .padding()
-            
-            // Use image for background
-            Text("hello world :3")
-                .frame(width: 300, height: 300)
-                .foregroundColor(.white)
-                .background(
-                    GeometryReader { geo in
-                        let geoWidth = geo.size.width
-                        let geoHeight = geo.size.height
-                        
-                        Image("pexels-pixabay-68474")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: geoWidth * 0.8, height: geoHeight * 0.8)
-                            .frame(width: geoWidth, height: geoHeight)
-                    }
-                )
-                .background(.blue)
-                .padding()
-
+            }
         }
-        .padding()
     }
 }
 
