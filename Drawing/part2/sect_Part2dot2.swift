e42cb228e21ead6fb3b3d1efd0bb6320a89e98cf
SeikaHirori <SeikaHirori@outlook.com>
2023-05-04 15:09:11 -0700

    Added ImagePaint to project

--- Drawing/part2/sect_Part2dot2.swift
+++ Drawing/part2/sect_Part2dot2.swift
@@ -15,36 +15,43 @@ struct sect_Part2dot2: View {
             
             ScrollView{
                 VStack {
-                    // Background modifier
-                    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                        .frame(width: 300, height: 300)
-                        .background(Color.red)
-                        .padding()
                     
-                    // Border modifier
-                    Text("hello world :3")
-                        .frame(width: 300, height: 300)
-                        .border(.red, width: 30)
-                        .padding()
+//                    // Background modifier
+//                    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//                        .frame(width: 300, height: 300)
+//                        .background(Color.red)
+//                        .padding()
+//
+//                    // Border modifier
+//                    Text("hello world :3")
+//                        .frame(width: 300, height: 300)
+//                        .border(.red, width: 30)
+//                        .padding()
+//
+//                    // Use image for background
+//                    Text("hello world :3")
+//                        .frame(width: 300, height: 300)
+//                        .foregroundColor(.white)
+//                        .background(
+//                            GeometryReader { geo in
+//                                let geoWidth = geo.size.width
+//                                let geoHeight = geo.size.height
+//
+//                                Image("pexels-pixabay-68474")
+//                                    .resizable()
+//                                    .scaledToFit()
+//                                    .frame(width: geoWidth * 0.8, height: geoHeight * 0.8)
+//                                    .frame(width: geoWidth, height: geoHeight)
+//                            }
+//                        )
+//                        .background(.blue)
+//                        .padding()
+//                        .padding()
                     
-                    // Use image for background
-                    Text("hello world :3")
+                    // Using ImagePaint
+                    Text("Hello worldddd :3")
                         .frame(width: 300, height: 300)
-                        .foregroundColor(.white)
-                        .background(
-                            GeometryReader { geo in
-                                let geoWidth = geo.size.width
-                                let geoHeight = geo.size.height
-                                
-                                Image("pexels-pixabay-68474")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: geoWidth * 0.8, height: geoHeight * 0.8)
-                                    .frame(width: geoWidth, height: geoHeight)
-                            }
-                        )
-                        .background(.blue)
-                        .padding()
+                        .border(ImagePaint(image: Image("Sunny_green_paddy_fields_with_trees_and_long_shadows_at_golden_hour"),scale: 0.1), width: 30)  
                 }
                 .frame(width: outerGeoWidth * 0.90, height: outerGeoHeight * 0.90)
                 .padding()
--- _images/creditsImages.md
+++ _images/creditsImages.md
@@ -1,2 +1,5 @@
 #  <#Title#>
+Credits for images used in project.
 
+- [pexels-pixabay-68474](https://www.pexels.com/photo/blue-water-68474/)
+- [Sunny_green_paddy_fields_with_trees_and_long_shadows_at_golden_hour] (https://commons.wikimedia.org/wiki/File:Sunny_green_paddy_fields_with_trees_and_long_shadows_at_golden_hour.jpg)
