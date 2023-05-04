863f7b84dfb49cdc9791fbb4e79aa5580ad2e15e
SeikaHirori <SeikaHirori@outlook.com>
2023-05-04 14:07:02 -0700

    Added modifiers to Text

--- Drawing/part2/sect_Part2dot2.swift
+++ Drawing/part2/sect_Part2dot2.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct sect_Part2dot2: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        return VStack {
+            
+            // Background modifier
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .frame(width: 300, height: 300)
+                .background(Color.red)
+                .padding()
+            
+            // Border modifier
+            Text("hello world :3")
+                .frame(width: 300, height: 300)
+                .border(.red, width: 30)
+        }
+        
+        
     }
 }
 
