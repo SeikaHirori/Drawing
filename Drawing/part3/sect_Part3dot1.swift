aa6ece9e5b05b6a1f1567058acd2f2b0f94e6638
SeikaHirori <SeikaHirori@outlook.com>
2023-05-07 15:10:04 -0700

    Created version that uses pure RGB

--- Drawing/part3/sect_Part3dot1.swift
+++ Drawing/part3/sect_Part3dot1.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct sect_Part3dot1: View {
     var body: some View {
         return VStack {
-            BrighterStuff_v1()
+            BrighterStuff_v2()
         }
     }
 }
@@ -51,8 +51,51 @@ struct RedRectangle_v2_shortCut: View {
     }
 }
 
+/// Uses SwiftUI's adaptive interpretation of RGB
 /// Example code: https://www.hackingwithswift.com/books/ios-swiftui/special-effects-in-swiftui-blurs-blending-and-more#:~:text=As%20an%20example%2C%20we%20could%20render%20three%20circles%20at%20various%20positions%20inside%20a%20ZStack%2C%20then%20use%20a%20slider%20to%20control%20their%20size%20and%20overlap%3A
 struct BrighterStuff_v1: View {
+    @State private var amount:Double = 0.00 // Setting this default value to see if Circle exists in view for debug
+    
+    var body: some View {
+        return VStack {
+            ZStack {
+                Circle()
+                    .fill(Color(red: 1, green: 0, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: -50, y: -80)
+                    .blendMode(.screen)
+
+                Circle()
+                    .fill(Color(red: 0, green: 1, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: 50, y: -80)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(Color(red: 0, green: 0, blue: 1))
+                    .frame(width: 200 * amount)
+                    .blendMode(.screen)
+                
+            }
+            .frame(width: 300, height: 300)
+            
+            Slider(value: $amount)
+                .padding()
+                .onChange(of: amount) { _ in
+                    print("Slider amount: \(amount)")
+                } // Debug
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black)
+        .ignoresSafeArea()
+            
+    }
+}
+
+
+/// Uses pure color of RGB
+/// Example code: https://www.hackingwithswift.com/books/ios-swiftui/special-effects-in-swiftui-blurs-blending-and-more#:~:text=If%20you%20want%20to%20see%20the%20full%20effect%20of%20blending%20red%2C%20green%2C%20and%20blue%2C%20you%20should%20use%20custom%20colors%20like%20these%20three%3A
+struct BrighterStuff_v2: View {
     @State private var amount:Double = 0.00 // Setting this default value to see if Circle exists in view for debug
     
     var body: some View {
