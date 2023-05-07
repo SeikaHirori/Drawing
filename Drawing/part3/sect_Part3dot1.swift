9927a6a2884cf2ecfe8d483b360727ddddc4e92b
SeikaHirori <SeikaHirori@outlook.com>
2023-05-07 15:03:05 -0700

    Added 3 Circles that will blend into each other when the slider is moved to max

--- Drawing/part3/sect_Part3dot1.swift
+++ Drawing/part3/sect_Part3dot1.swift
@@ -51,13 +51,42 @@ struct RedRectangle_v2_shortCut: View {
     }
 }
 
+/// Example code: https://www.hackingwithswift.com/books/ios-swiftui/special-effects-in-swiftui-blurs-blending-and-more#:~:text=As%20an%20example%2C%20we%20could%20render%20three%20circles%20at%20various%20positions%20inside%20a%20ZStack%2C%20then%20use%20a%20slider%20to%20control%20their%20size%20and%20overlap%3A
 struct BrighterStuff_v1: View {
+    @State private var amount:Double = 0.00 // Setting this default value to see if Circle exists in view for debug
+    
     var body: some View {
         return VStack {
             ZStack {
-                Text("hello world")
+                Circle()
+                    .fill(.red)
+                    .frame(width: 200 * amount)
+                    .offset(x: -50, y: -80)
+                    .blendMode(.screen)
+
+                Circle()
+                    .fill(.green)
+                    .frame(width: 200 * amount)
+                    .offset(x: 50, y: -80)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(.blue)
+                    .frame(width: 200 * amount)
+                    .blendMode(.screen)
+                
             }
             .frame(width: 300, height: 300)
+            
+            Slider(value: $amount)
+                .padding()
+                .onChange(of: amount) { _ in
+                    print("Slider amount: \(amount)")
+                } // Debug
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black)
+        .ignoresSafeArea()
+            
     }
 }
