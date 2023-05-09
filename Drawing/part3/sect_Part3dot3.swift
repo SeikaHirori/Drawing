700e1980809e017948f2e2da9bbf92b66a020907
SeikaHirori <SeikaHirori@outlook.com>
2023-05-09 12:43:48 -0700

    created outline of struct "Checkerboard"; need to finish adding code

--- Drawing/part3/sect_Part3dot3.swift
+++ Drawing/part3/sect_Part3dot3.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 
 struct sect_Part3dot3: View {
+    @State private var rows: Int = 4
+    @State private var columns: Int = 4
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        return VStack {
+            Checkboard(rows: rows, columns: columns)
+                .onTapGesture {
+                    withAnimation(.linear(duration: 3)) {
+                        rows = 8
+                        columns = 16
+                    }
+                }
+        }
     }
 }
 
@@ -17,4 +29,25 @@ struct sect_Part3dot3_Previews: PreviewProvider {
     static var previews: some View {
         sect_Part3dot3()
     }
+}
+
+struct Checkboard: Shape {
+    var rows: Int
+    var columns: Int
+    
+    func path(in rect: CGRect) -> Path {
+        var path:Path = Path()
+        
+        // figure out how big each row/ column needs to be
+        let rowSize: Double = rect.height / Double(rows)
+        let columnSize: Double = rect.width / Double(columns)
+        
+        // loop over all rows and columns, making alternating squares colored
+        
+        
+        
+        
+        return path
+    }
+    
 }
