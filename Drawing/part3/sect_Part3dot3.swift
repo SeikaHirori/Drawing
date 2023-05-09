a5574b6d906a53b6dd5f5d0dd9fbfa03d7df5de8
SeikaHirori <SeikaHirori@outlook.com>
2023-05-09 12:40:52 -0700

    Started section 3.3 and created outline of struct "Checkerboard"

---
+++ Drawing/part3/sect_Part3dot3.swift
@@ -0,0 +1,20 @@
+//
+//  sect_Part3dot3.swift
+//  Drawing
+//
+//  Created by Seika Hirori on 5/9/23.
+//
+
+import SwiftUI
+
+struct sect_Part3dot3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct sect_Part3dot3_Previews: PreviewProvider {
+    static var previews: some View {
+        sect_Part3dot3()
+    }
+}
